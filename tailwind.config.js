156445ee20f8f169ce1bdf0356aea783765b761f
Md Shakil Chowdhury <shakil.singularity@gmail.com>
2023-07-21 08:19:11 +0600

    3rd

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,40 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs: "400px",
+      // => @media (min-width: 400px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+    extend: {
+      fontSize: {
+        "7xl": "5rem",
+      },
+    },
   },
   plugins: [],
 };
