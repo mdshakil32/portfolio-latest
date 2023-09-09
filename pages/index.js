06a1bb80901e51acb3ab37b5a1d62c8262facdb9
shakil <shakil.singularity@gmail.com>
2023-09-09 14:15:42 +0600

    :construction: content updated

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="overflow-hidden">
-        {/* <Intro /> */}
+        <Intro />
         <Header />
         <Banner />
         <About />
